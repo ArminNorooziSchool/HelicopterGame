fb9758f636ed93d37e4e1dd38a42c05132b15f3d
ArminNorooziSchool <124630843+ArminNorooziSchool@users.noreply.github.com>
2023-06-08 13:39:13 -0600

    Add files via upload

---
+++ flappymain.js
@@ -0,0 +1,60 @@
+// Helicopter Game Start
+
+// Set up canvas and graphics context
+let cnv = document.getElementById("my-canvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+// Global Variables
+let heliImg = document.createElement("img");
+heliImg.src = "img/heliBlueTransparent.png";
+
+let explosion = document.createElement("audio");
+explosion.src = "sound/explosion.wav";
+
+let propeller = document.createElement("audio");
+propeller.src = "sound/propeller.wav";
+
+let mouseIsPressed = false;
+
+let state;
+let heli;
+let wall1, wall2, wall3;
+reset();
+
+// Draw Function
+window.addEventListener("load", draw);
+
+function draw() {
+  if (state === "start") {
+    drawStart();
+  } else if (state === "gameon") {
+    runGame();
+  } else if (state === "gameover") {
+    drawGameOver();
+  }
+
+  // Request Animation Frame
+  requestAnimationFrame(draw);
+}
+
+// Event Stuff
+document.addEventListener("mousedown", mousedownHandler);
+document.addEventListener("mouseup", mouseupHandler);
+
+function mousedownHandler() {
+  mouseIsPressed = true;
+
+  propeller.play();
+
+  if (state === "start") {
+    state = "gameon";
+  } else if (state === "gameover") {
+    state = "start";
+  }
+}
+
+function mouseupHandler() {
+  mouseIsPressed = false;
+}
